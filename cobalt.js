b965c42d7e387d2b617fbd13cbab9ad9cdd69ff4
Guillaume Gendre <dzb.rtz@gmail.com>
2016-05-31 15:32:28 +0200

    update version number

--- cobalt.js
+++ cobalt.js
@@ -22,7 +22,7 @@
  * THE SOFTWARE.
  */
 var cobalt = {
-    version: '0.5.0',
+    version: '0.5.1',
     events: {}, //objects of events defined by the user
     debug: false,
     debugInBrowser: false,
